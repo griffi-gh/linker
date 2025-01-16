4b28c491b6af6ebfa186059093659c6e0f25573d
Gerg-L <GregLeyda@proton.me>
2025-01-16 18:49:00 -0500

    flake: add other systems

--- flake.nix
+++ flake.nix
@@ -1,34 +1,53 @@
 {
 
-  inputs.nixpkgs = {
-    type = "github";
-    owner = "NixOS";
-    repo = "nixpkgs";
-    ref = "nixos-unstable";
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+    systems = {
+      type = "github";
+      owner = "nix-systems";
+      repo = "default";
+    };
+
   };
 
   outputs =
-    { nixpkgs, self }:
     {
-      packages.x86_64-linux =
+      nixpkgs,
+      self,
+      systems,
+    }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = eachSystem (
+        system:
         let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
           linker = pkgs.callPackage ./package.nix { };
-          default = self.packages.x86_64-linux.linker;
-        };
-      devShells.x86_64-linux =
+          default = self.packages.${system}.linker;
+        }
+      );
+      devShells = eachSystem (
+        system:
         let
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
           default = pkgs.mkShell {
-            inputsFrom = [ self.packages.${pkgs.stdenv.system}.default ];
+            inputsFrom = [ self.packages.${system}.default ];
             packages = builtins.attrValues {
               inherit (pkgs) rust-analyzer rustfmt clippy;
             };
           };
-        };
+        }
+      );
     };
 }
